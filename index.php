e96028eae5dd895c97dd274a406cd3c497fb1ef1
Gage Ames <gra5028@psu.edu>
2014-02-08 17:26:48 -0500

    Actually use the provider ID when getting results.  :)

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         // Try to infer a category from the message
         $category = tryExtractCategory($body);
 
-        $suggestion = getShowSuggestion($user["zip_code"], $category);
+        $suggestion = getShowSuggestion($user["service_id"], $category);
         
         if ($suggestion)
         {
@@ -127,17 +127,8 @@
 //    $num_rows = count($data);
 //  } else { db_print_error(); }
   
-  function getShowSuggestion($zip, $category)
+  function getShowSuggestion($serviceId, $category)
   {
-
-    $jsonurl = "http://api.rovicorp.com/TVlistings/v9/listings/services/postalcode/$zip/info?locale=en-US&countrycode=US&apikey=bnp966tdms7t9p5hze264wae&sig=sig";
-
-    $json = file_get_contents($jsonurl);
-
-    $resultObj = json_decode($json);
-
-    $serviceId = $resultObj->ServicesResult->Services->Service[0]->ServiceId;
-
     $jsonurl = "http://api.rovicorp.com/TVlistings/v9/listings/linearschedule/$serviceId/info?locale=en-US&duration=30&inprogress=true&apikey=tq9qyz3r86vjhqn9w49vf4dt&sig=sig";
     $json = file_get_contents($jsonurl);
     $resultObj = json_decode($json);
