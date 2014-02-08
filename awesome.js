f66499c2d6f4660709aa1a5caf3f0609420f8e7a
Kyle Ribordy <kyle@kdribordy.com>
2014-02-08 07:54:28 +0000

    Twilio now parrots back the incoming SMS

--- awesome.js
+++ awesome.js
@@ -1,13 +1,25 @@
 var twilio = require('twilio'),
     http = require('http');
+
+var qs = require('querystring');
  
 http.createServer(function (req, res) {
-    var resp = new twilio.TwimlResponse();
-    resp.sms('ahoy hoy! Testing Twilio and node.js');
-    res.writeHead(200, {
-        'Content-Type':'text/xml'
+    var body = '';
+    req.on('data', function(data) {
+      body += data;
     });
-    res.end(resp.toString());
+
+    req.on('end', function() {
+      var parsed_body = qs.parse(body);
+
+      var resp = new twilio.TwimlResponse();
+      resp.sms(parsed_body['Body']);
+      res.writeHead(200, {
+          'Content-Type':'text/xml'
+      });
+      res.end(resp.toString());
+    });
+
 }).listen(1337);
  
 console.log('Visit http://localhost:1337/ in your browser to see your TwiML document!');
