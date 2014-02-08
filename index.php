18b47b5fe50374a4c941c7b74dc1677220764659
Kyle Ribordy <kyle@izzo.(none)>
2014-02-08 09:53:22 +0000

    Grabs zip from text and displays relevant airings

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
   header("content-type: text/xml");
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
-  $jsonurl = "http://api.rovicorp.com/TVlistings/v9/listings/linearschedule/360861/info?locale=en-US&duration=30&inprogress=true&apikey=tq9qyz3r86vjhqn9w49vf4dt&sig=sig";
+  $zip = $_REQUEST["Body"] ? $_REQUEST["Body"] : 46385;
+
+  $jsonurl = "http://api.rovicorp.com/TVlistings/v9/listings/services/postalcode/$zip/info?locale=en-US&countrycode=US&apikey=bnp966tdms7t9p5hze264wae&sig=sig";
+
+  $json = file_get_contents($jsonurl);
+
+  $resultObj = json_decode($json);
+
+  $serviceId = $resultObj->ServicesResult->Services->Service[0]->ServiceId;
+
+  $jsonurl = "http://api.rovicorp.com/TVlistings/v9/listings/linearschedule/$serviceId/info?locale=en-US&duration=30&inprogress=true&apikey=tq9qyz3r86vjhqn9w49vf4dt&sig=sig";
   $json = file_get_contents($jsonurl);
   $resultObj = json_decode($json);
+
+  $airings = $resultObj->LinearScheduleResult->Schedule->Airings;
 
   foreach ($resultObj as $item)
   {
@@ -12,5 +24,5 @@
   }
 ?>
 <Response>
-    <Message></Message>
+  <Message></Message>
 </Response>
