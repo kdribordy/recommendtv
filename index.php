16e70f2cf64b2bd01fe482ba96d4f037d3a67dc6
root <root@d1stkfactory>
2014-02-08 09:18:38 +0000

    Changed data source filter calling.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   $jsonurl = "http://api.rovicorp.com/TVlistings/v9/listings/linearschedule/360861/info?locale=en-US&duration=30&inprogress=true&apikey=tq9qyz3r86vjhqn9w49vf4dt&sig=sig";
   $json = file_get_contents($jsonurl);
   $resultObj = json_decode($json);
+  filterByCategory($resultObj.LinearScheduleResult.Schedule.Airings, "Other");
 
   foreach ($resultObj as $item)
   {
