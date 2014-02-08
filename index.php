2efaf5c448fdb1690970d9a8354595468dffb4e5
root <root@d1stkfactory>
2014-02-08 09:01:11 +0000

    Added initial PHP version. Gets from endpoint.

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  header("content-type: text/xml");
+  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+  $jsonurl = "http://api.rovicorp.com/TVlistings/v9/listings/linearschedule/360861/info?locale=en-US&duration=30&inprogress=true&apikey=tq9qyz3r86vjhqn9w49vf4dt&sig=sig";
+  $json = file_get_contents($jsonurl);
+  $resultObj = json_decode($json);
+
+  foreach ($resultObj as $item)
+  {
+    
+  }
+?>
+<Response>
+    <Message></Message>
+</Response>
