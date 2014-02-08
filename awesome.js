cdc2d33cf35ccdd1f1678be7c3017b6d7dce9ea5
Kyle Ribordy <kyle@kdribordy.com>
2014-02-08 08:24:39 +0000

    Working but ugly as sin

--- awesome.js
+++ awesome.js
@@ -11,15 +11,36 @@ http.createServer(function (req, res) {
 
     req.on('end', function() {
       var parsed_body = qs.parse(body);
+      var zip = parsed_body['Body'];
 
-      var resp = new twilio.TwimlResponse();
-      resp.sms(parsed_body['Body']);
-      res.writeHead(200, {
-          'Content-Type':'text/xml'
+      var options = {
+        host: 'api.rovicorp.com',
+        port: 80,
+        path: '/TVlistings/v9/listings/services/postalcode/'+zip+'/info?locale=en-US&countrycode=US&apikey=bnp966tdms7t9p5hze264wae&sig=sig',
+        method: 'GET'
+      };
+      var get_providers_req = http.get(options, function(res1) {
+        var page_data = "";
+        res1.setEncoding('utf8');
+        res1.on('data', function (chunk) {
+          page_data += chunk;
+        });
+        parsed_page_data = qs.parse(page_data);
+        res1.on('end', function () {
+          var json = JSON.parse(page_data);
+          var service_id = json.ServicesResult.Services.Service[0].ServiceId;
+          console.log(service_id);
+
+          var resp = new twilio.TwimlResponse();
+          resp.sms(service_id);
+          res.writeHead(200, {
+              'Content-Type':'text/xml'
+          });
+          res.end(resp.toString());
+        });
       });
-      res.end(resp.toString());
+      get_providers_req.end();
     });
-
 }).listen(1337);
  
 console.log('Visit http://localhost:1337/ in your browser to see your TwiML document!');
